68637063c357c70cd022ee0143830128473841da
aaron saunders <aaronksaunders@gmail.com>
2023-08-30 15:30:30 -0400

    first commit

---
+++ app.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const { $trpcClient } = useNuxtApp();
+console.log($trpcClient);
+
+const { data } = await $trpcClient.hello.useQuery({ text: "smith" });
+</script>
+
+<template>
+  <p>app</p>
+  <div>
+    <p>{{ data?.greeting }}</p>
+  </div>
+</template>
